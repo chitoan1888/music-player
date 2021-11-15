65c698bbc3108c1c04082e523469c9bd23c577e6
chitoankl1888 <tn414288@gmail.com>
2021-11-15 13:29:50 +0700

    change database

--- musicplayer/settings.py
+++ musicplayer/settings.py
@@ -91,16 +91,12 @@ WSGI_APPLICATION = 'musicplayer.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'music-app-database',
+        'USER': 'myadmin@music-app-database',
+        'PASSWORD': 'Hannie@16879',
+        'HOST': 'music-app-database.postgres.database.azure.com',
+        'PORT': '5432',
     },
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': config('DB_NAME'),
-    #     'USER': config('DB_USER'),
-    #     'PASSWORD': config('DB_PASSWORD'),
-    #     'HOST': config('DB_HOST'),
-    #     'PORT': '5432'
-    # }
 }
 
 
